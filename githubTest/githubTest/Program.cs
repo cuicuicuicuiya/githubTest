5e094be99035de5e6cffcfbf88b73c307636371c
cc兮 <404533004@qq.com>
2017-07-20 09:33:06 +0800

    Add files via upload

---
+++ githubTest/githubTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace githubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("第一次用的。。github。");
+            Console.Read();
+        }
+    }
+}
